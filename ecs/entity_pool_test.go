dfd2a9ae43a6664f9c681fd27b05954304983c7a
mlange-42 <martin_lange_@gmx.net>
2023-02-05 00:32:28 +0100

    add more tests

--- ecs/entity_pool_test.go
+++ ecs/entity_pool_test.go
@@ -1,6 +1,7 @@
 package ecs
 
 import (
+	"math/rand"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -16,19 +17,19 @@ func TestEntityPool(t *testing.T) {
 	}
 	assert.Equal(t, expectedAll, p.entities, "Wrong initial entities")
 
-	e1 := p.entities[1]
-	p.Recycle(e1)
-	assert.False(t, p.Alive(e1), "Dead entity should not be alive")
+	e0 := p.entities[0]
+	p.Recycle(e0)
+	assert.False(t, p.Alive(e0), "Dead entity should not be alive")
 
-	e1Old := e1
-	e1 = p.Get()
+	e0Old := e0
+	e0 = p.Get()
 	expectedAll[1].gen++
-	assert.True(t, p.Alive(e1), "Recycled entity of new generation should be alive")
-	assert.False(t, p.Alive(e1Old), "Recycled entity of old generation should not be alive")
+	assert.True(t, p.Alive(e0), "Recycled entity of new generation should be alive")
+	assert.False(t, p.Alive(e0Old), "Recycled entity of old generation should not be alive")
 
 	assert.Equal(t, expectedAll, p.entities, "Wrong entities after get/recycle")
 
-	e0Old := p.entities[0]
+	e0Old = p.entities[0]
 	for i := 0; i < 5; i++ {
 		p.Recycle(p.entities[i])
 		expectedAll[i].gen++
@@ -41,4 +42,32 @@ func TestEntityPool(t *testing.T) {
 	}
 
 	assert.False(t, p.Alive(e0Old), "Recycled entity of old generation should not be alive")
+}
+
+func TestEntityPoolStochastic(t *testing.T) {
+	p := newImplicitListEntityPool()
+
+	alive := map[Entity]bool{}
+	for i := 0; i < 100; i++ {
+		e := p.Get()
+		alive[e] = true
+	}
+	for e := range alive {
+		if rand.Float32() > 0.5 {
+			continue
+		}
+		p.Recycle(e)
+		alive[e] = false
+	}
+	for e, a := range alive {
+		assert.Equal(t, a, p.Alive(e), "Wrong alive state of entity %v after removal", e)
+	}
+	for i := 0; i < 100; i++ {
+		e := p.Get()
+		alive[e] = true
+	}
+	for e, a := range alive {
+		assert.Equal(t, a, p.Alive(e), "Wrong alive state of entity %v after recycling", e)
+	}
+	assert.Equal(t, uint32(0), p.available, "No more entities should be available")
 }
