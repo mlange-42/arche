36011c19d98c850120f5696a8706aa3c26bb76be
mlange-42 <martin_lange_@gmx.net>
2023-02-09 23:54:44 +0100

    add benchmarks for accessing multiple components

--- benchmark/iterate/arche_test.go
+++ benchmark/iterate/arche_test.go
@@ -30,6 +30,37 @@ func runArcheQuery(b *testing.B, count int) {
 	}
 }
 
+func runArcheQuery5C(b *testing.B, count int) {
+	b.StopTimer()
+	world := ecs.NewWorld()
+
+	id0 := ecs.ComponentID[testStruct0](&world)
+	id1 := ecs.ComponentID[testStruct1](&world)
+	id2 := ecs.ComponentID[testStruct2](&world)
+	id3 := ecs.ComponentID[testStruct3](&world)
+	id4 := ecs.ComponentID[testStruct4](&world)
+
+	for i := 0; i < count; i++ {
+		entity := world.NewEntity()
+		world.Add(entity, id0, id1, id2, id3, id4)
+	}
+	b.StartTimer()
+
+	for i := 0; i < b.N; i++ {
+		b.StopTimer()
+		query := world.Query(id0, id1, id2, id3, id4)
+		b.StartTimer()
+		for query.Next() {
+			t1 := (*testStruct0)(query.Get(id0))
+			t2 := (*testStruct0)(query.Get(id1))
+			t3 := (*testStruct0)(query.Get(id2))
+			t4 := (*testStruct0)(query.Get(id3))
+			t5 := (*testStruct0)(query.Get(id4))
+			_, _, _, _, _ = t1, t2, t3, t4, t5
+		}
+	}
+}
+
 func runArcheQueryGeneric(b *testing.B, count int) {
 	b.StopTimer()
 	world := ecs.NewWorld()
@@ -50,6 +81,33 @@ func runArcheQueryGeneric(b *testing.B, count int) {
 		for query.Next() {
 			pos := query.Get1()
 			_ = pos
+		}
+	}
+}
+
+func runArcheQueryGeneric5C(b *testing.B, count int) {
+	b.StopTimer()
+	world := ecs.NewWorld()
+
+	id0 := ecs.ComponentID[testStruct0](&world)
+	id1 := ecs.ComponentID[testStruct1](&world)
+	id2 := ecs.ComponentID[testStruct2](&world)
+	id3 := ecs.ComponentID[testStruct3](&world)
+	id4 := ecs.ComponentID[testStruct4](&world)
+
+	for i := 0; i < count; i++ {
+		entity := world.NewEntity()
+		world.Add(entity, id0, id1, id2, id3, id4)
+	}
+	b.StartTimer()
+
+	for i := 0; i < b.N; i++ {
+		b.StopTimer()
+		query := ecs.Query5[testStruct0, testStruct1, testStruct2, testStruct3, testStruct4](&world)
+		b.StartTimer()
+		for query.Next() {
+			t1, t2, t3, t4, t5 := query.GetAll()
+			_, _, _, _, _ = t1, t2, t3, t4, t5
 		}
 	}
 }
@@ -176,6 +234,18 @@ func BenchmarkArcheIterQuery_100_000(b *testing.B) {
 	runArcheQuery(b, 100000)
 }
 
+func BenchmarkArcheIterQuery5C_1_000(b *testing.B) {
+	runArcheQuery5C(b, 1000)
+}
+
+func BenchmarkArcheIterQuery5C_10_000(b *testing.B) {
+	runArcheQuery5C(b, 10000)
+}
+
+func BenchmarkArcheIterQuery5C_100_000(b *testing.B) {
+	runArcheQuery5C(b, 100000)
+}
+
 func BenchmarkArcheIterQueryGeneric_1_000(b *testing.B) {
 	runArcheQueryGeneric(b, 1000)
 }
@@ -186,6 +256,18 @@ func BenchmarkArcheIterQueryGeneric_10_000(b *testing.B) {
 
 func BenchmarkArcheIterQueryGeneric_100_000(b *testing.B) {
 	runArcheQueryGeneric(b, 100000)
+}
+
+func BenchmarkArcheIterQueryGeneric5C_1_000(b *testing.B) {
+	runArcheQueryGeneric5C(b, 1000)
+}
+
+func BenchmarkArcheIterQueryGeneric5C_10_000(b *testing.B) {
+	runArcheQueryGeneric5C(b, 10000)
+}
+
+func BenchmarkArcheIterQueryGeneric5C_100_000(b *testing.B) {
+	runArcheQueryGeneric5C(b, 100000)
 }
 
 func BenchmarkArcheIterQueryEntity_1_000(b *testing.B) {
--- benchmark/iterate/ggecs_test.go
+++ benchmark/iterate/ggecs_test.go
@@ -30,6 +30,37 @@ func runGameEngineEcs(b *testing.B, count int) {
 	}
 }
 
+func runGameEngineEcs5C(b *testing.B, count int) {
+	b.StopTimer()
+	comps := []ecs.ComponentConfig{
+		{ID: 0, Component: testStruct0{}},
+		{ID: 1, Component: testStruct1{}},
+		{ID: 2, Component: testStruct2{}},
+		{ID: 3, Component: testStruct3{}},
+		{ID: 4, Component: testStruct4{}},
+	}
+	world := ecs.NewWorld(comps...)
+
+	for i := 0; i < count; i++ {
+		entity := world.NewEntity()
+		world.Assign(entity, 0, 1, 2, 3, 4)
+	}
+	filter := world.NewFilter(0, 1, 2, 3, 4)
+
+	b.StartTimer()
+
+	for i := 0; i < b.N; i++ {
+		for _, e := range filter.Entities() {
+			t1 := (*testStruct0)(world.Component(e, 0))
+			t2 := (*testStruct1)(world.Component(e, 1))
+			t3 := (*testStruct2)(world.Component(e, 2))
+			t4 := (*testStruct3)(world.Component(e, 3))
+			t5 := (*testStruct4)(world.Component(e, 4))
+			_, _, _, _, _ = t1, t2, t3, t4, t5
+		}
+	}
+}
+
 func runGameEngineEcs1kArch(b *testing.B, count int) {
 	b.StopTimer()
 	comps := []ecs.ComponentConfig{
@@ -86,6 +117,18 @@ func BenchmarkGGEcsIter_10_000(b *testing.B) {
 
 func BenchmarkGGEcsIter_100_000(b *testing.B) {
 	runGameEngineEcs(b, 100000)
+}
+
+func BenchmarkGGEcsIter5C_1_000(b *testing.B) {
+	runGameEngineEcs5C(b, 1000)
+}
+
+func BenchmarkGGEcsIter5C_10_000(b *testing.B) {
+	runGameEngineEcs5C(b, 10000)
+}
+
+func BenchmarkGGEcsIter5C_100_000(b *testing.B) {
+	runGameEngineEcs5C(b, 100000)
 }
 
 func BenchmarkGGEcsIter1kArch_1_000(b *testing.B) {
