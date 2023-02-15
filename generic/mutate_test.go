0932f4ba7c00caba909381ffdf058a2278c2484d
mlange-42 <martin_lange_@gmx.net>
2023-02-15 23:22:26 +0100

    add tests for generic multi-map

--- generic/mutate_test.go
+++ generic/mutate_test.go
@@ -13,8 +13,12 @@ func TestMutate(t *testing.T) {
 
 	mut := NewMutate(&w).WithAdd(T2[testStruct0, testStruct1]()...)
 	mutR := NewMutate(&w).WithRemove(T2[testStruct0, testStruct1]()...)
+	mutEx := NewMutate(&w).
+		WithRemove(T2[testStruct0, testStruct1]()...).
+		WithAdd(T2[testStruct2, testStruct3]()...)
 
 	mapper := NewMap2[testStruct0, testStruct1](&w)
+	mapper2 := NewMap2[testStruct2, testStruct3](&w)
 	map0 := NewMap[testStruct0](&w)
 	map1 := NewMap[testStruct1](&w)
 
@@ -31,4 +35,12 @@ func TestMutate(t *testing.T) {
 	s0, s1 = mapper.Get(e)
 	assert.NotNil(t, s0)
 	assert.NotNil(t, s1)
+
+	mutEx.Exchange(e)
+	s0, s1 = mapper.Get(e)
+	assert.Nil(t, s0)
+	assert.Nil(t, s1)
+	s2, s3 := mapper2.Get(e)
+	assert.NotNil(t, s2)
+	assert.NotNil(t, s3)
 }
