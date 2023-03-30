fa0b4b1dad1c220aced2c771313f9cbe622800f8
Martin Lange <44003176+mlange-42@users.noreply.github.com>
2023-03-30 18:58:47 +0200

    simplify/fix relations benchmarks by removing additional parent mapper (#195)

--- benchmark/arche/relations/parent_array_test.go
+++ benchmark/arche/relations/parent_array_test.go
@@ -13,10 +13,9 @@ func benchmarkParentArray(b *testing.B, numParents int) {
 	world := ecs.NewWorld(ecs.NewConfig().WithCapacityIncrement(1024))
 
 	parentMapper := generic.NewMap1[ParentArr](&world)
-	parentMapperFull := generic.NewMap1[ParentArr](&world)
 	childMapper := generic.NewMap1[Child](&world)
 
-	spawnedPar := parentMapperFull.NewEntitiesQuery(numParents)
+	spawnedPar := parentMapper.NewEntitiesQuery(numParents)
 	parents := make([]ecs.Entity, 0, numParents)
 	for spawnedPar.Next() {
 		parents = append(parents, spawnedPar.Entity())
