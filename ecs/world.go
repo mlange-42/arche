990c7f7265bb0ad8f47845cd592be0e956e738d5
mlange-42 <martin_lange_@gmx.net>
2023-03-30 21:43:23 +0200

    simplify archetype graph code

--- ecs/world.go
+++ ecs/world.go
@@ -711,23 +711,23 @@ func (w *World) findOrCreateArchetype(start *archetype, add []ID, rem []ID) *arc
 	mask := start.Mask
 	for _, id := range rem {
 		mask.Set(id, false)
-		if next, ok := curr.GetTransitionRemove(id); ok {
+		if next, ok := curr.TransitionRemove.Get(id); ok {
 			curr = next
 		} else {
 			next, _ := w.findOrCreateArchetypeSlow(mask)
-			next.SetTransitionAdd(id, curr)
-			curr.SetTransitionRemove(id, next)
+			next.TransitionAdd.Set(id, curr)
+			curr.TransitionRemove.Set(id, next)
 			curr = next
 		}
 	}
 	for _, id := range add {
 		mask.Set(id, true)
-		if next, ok := curr.GetTransitionAdd(id); ok {
+		if next, ok := curr.TransitionAdd.Get(id); ok {
 			curr = next
 		} else {
 			next, _ := w.findOrCreateArchetypeSlow(mask)
-			next.SetTransitionRemove(id, curr)
-			curr.SetTransitionAdd(id, next)
+			next.TransitionRemove.Set(id, curr)
+			curr.TransitionAdd.Set(id, next)
 			curr = next
 		}
 	}
