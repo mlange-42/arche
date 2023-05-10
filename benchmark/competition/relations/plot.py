9c59d6cdefddbf7bca4d1c19441c9d171b47d247
mlange-42 <martin_lange_@gmx.net>
2023-05-10 22:51:14 +0200

    plot by parents and by children, fix plot legend location

--- benchmark/competition/relations/plot.py
+++ benchmark/competition/relations/plot.py
@@ -24,6 +24,7 @@ if __name__ == "__main__":
     data = data[data["Entities"] > 1000]
 
     models = ["ParentList", "ParentSlice", "Child", "Default", "Cached"]
+    entities = np.unique(data["Entities"])
     parents = np.unique(data["Parents"])
     children = np.unique(data["Children"])
 
@@ -44,51 +45,52 @@ if __name__ == "__main__":
     plt.rcParams["svg.fonttype"] = "none"
     plt.rcParams["font.family"] = "Arial"
 
-    fig, ax = plt.subplots(figsize=(6, 4))
-    ax.set_title("Iter & get 16 byte")
-    ax.set_xscale("log")
-    ax.set_yscale("log")
-    ax.set_xticks([10, 100, 1000, 10000])
-    ax.set_xticklabels([10, 100, 1000, 10000])
-    ax.set_yticks([1, 2, 5, 10, 20, 50, 100, 200])
-    ax.set_yticklabels([1, 2, 5, 10, 20, 50, 100, 200])
-    ax.set_xlabel("Number of parents", fontsize=11)
-    ax.set_ylabel("Time per Entity [ns]", fontsize=11)
+    for column, title, loc in [("Parents", "Parent entities", 2), ("Children", "Children per parent", 1)]:
+        fig, ax = plt.subplots(figsize=(6, 4))
+        ax.set_title("Benchmarks of ways to represent relations")
+        ax.set_xscale("log")
+        ax.set_yscale("log")
+        ax.set_xticks([10, 100, 1000, 10000])
+        ax.set_xticklabels([10, 100, 1000, 10000])
+        ax.set_yticks([1, 2, 5, 10, 20, 50, 100, 200])
+        ax.set_yticklabels([1, 2, 5, 10, 20, 50, 100, 200])
+        ax.set_xlabel(title, fontsize=11)
+        ax.set_ylabel("Time per Entity [ns]", fontsize=11)
 
-    ax.set_ylim(1, np.max(data["Time"]) * 1.2)
+        ax.set_ylim(1, np.max(data["Time"]) * 1.2)
 
-    for model in models:
-        mod_data = data[(data["Model"] == model)]
-        entities = np.unique(mod_data["Entities"])
-        for ent in entities:
-            extr = mod_data[mod_data["Entities"] == ent]
-            extr = extr.groupby("Parents").mean()
+        for model in models:
+            mod_data = data[(data["Model"] == model)]
+            count = np.unique(mod_data["Entities"])
+            for ent in count:
+                extr = mod_data[mod_data["Entities"] == ent]
+                extr = extr.groupby(column).mean()
 
+                line = linesEntities[ent]
+                ax.plot(
+                    extr.index,
+                    extr["Time"],
+                    linestyle=line[0],
+                    linewidth=line[1],
+                    color=colors[model],
+                    marker="o",
+                    markersize=3,
+                    label=model if ent == 100000 else None,
+                )
+        
+        for ent in reversed(entities):
             line = linesEntities[ent]
             ax.plot(
-                extr.index,
-                extr["Time"],
+                [0],
+                [0],
                 linestyle=line[0],
                 linewidth=line[1],
-                color=colors[model],
-                marker="o",
-                markersize=3,
-                label=model if ent == 100000 else None,
+                color="black",
+                label=f"{ent//1000}k entities",
             )
+        ax.legend(loc=loc)
+
+        fig.tight_layout()
+        fig.savefig(f"results-{column}.svg")
     
-    for ent in reversed(entities):
-        line = linesEntities[ent]
-        ax.plot(
-            [0],
-            [0],
-            linestyle=line[0],
-            linewidth=line[1],
-            color="black",
-            label=f"{ent//1000}k entities",
-        )
-
-    ax.legend()
-
-    fig.tight_layout()
-    fig.savefig("results.svg")
     plt.show()
