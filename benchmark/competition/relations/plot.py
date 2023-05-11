edde417da06bc69b9f19cfa6f1030fefc15cb7df
Martin Lange <44003176+mlange-42@users.noreply.github.com>
2023-05-12 00:23:34 +0200

    Benchmark plot with children on x axis and parents as line styles (#292)

--- benchmark/competition/relations/plot.py
+++ benchmark/competition/relations/plot.py
@@ -24,9 +24,12 @@ if __name__ == "__main__":
     data = data[data["Entities"] > 1000]
 
     models = ["ParentList", "ParentSlice", "Child", "Default", "Cached"]
-    entities = np.unique(data["Entities"])
-    parents = np.unique(data["Parents"])
-    children = np.unique(data["Children"])
+    
+    values = {
+        "Entities": np.unique(data["Entities"]),
+        "Parents": np.unique(data["Parents"]),
+        "Children": np.unique(data["Children"]),
+    }
 
     colors = {
         "Default": "blue",
@@ -35,11 +38,17 @@ if __name__ == "__main__":
         "ParentSlice": "red",
         "ParentList": "purple",
     }
-    linesEntities = {
+    lines_entities = {
         1000: ("dotted", 1.0),
         10000: ("dashed", 1.2),
         100000: ("solid", 1.5),
         1000000: ("solid", 2.5),
+    }
+    lines_parents = {
+        10000: ("dotted", 1.0),
+        1000: ("dashed", 1.2),
+        100: ("solid", 1.5),
+        10: ("solid", 2.5),
     }
     markers = {
         10: ("v", 25),
@@ -47,14 +56,16 @@ if __name__ == "__main__":
         1000: ("D", 20),
         10000: ("s", 20),
         100000: ("o", 30),
+        1000000: ("o", 50),
     }
 
     plt.rcParams["svg.fonttype"] = "none"
     plt.rcParams["font.family"] = "Arial"
 
-    for column, marker_column, title, loc, prim_values, sec_values in [
-        ("Parents", "Children", "Parent entities", 2, parents, children),
-        ("Children", "Parents", "Children per parent", 1, children, parents),
+    for line_column, x_column, marker_column, x_title, line_styles in [
+        ("Entities", "Parents", "Children", "Parent entities", lines_entities),
+        ("Entities", "Children", "Parents", "Children per parent", lines_entities),
+        ("Parents", "Children", "Entities", "Children per parent", lines_parents),
     ]:
         fig, ax = plt.subplots(figsize=(7, 4))
         ax.set_title("Benchmarks of ways to represent entity relations")
@@ -64,19 +75,19 @@ if __name__ == "__main__":
         ax.set_xticklabels([10, 100, 1000, 10000])
         ax.set_yticks([1, 2, 5, 10, 20, 50, 100, 200])
         ax.set_yticklabels([1, 2, 5, 10, 20, 50, 100, 200])
-        ax.set_xlabel(title, fontsize=11)
+        ax.set_xlabel(x_title, fontsize=11)
         ax.set_ylabel("Time per Entity [ns]", fontsize=11)
 
         ax.set_ylim(1, np.max(data["Time"]) * 1.2)
 
         for model in models:
             mod_data = data[(data["Model"] == model)]
-            count = np.unique(mod_data["Entities"])
+            count = np.unique(mod_data[line_column])
             for ent in count:
-                extr = mod_data[mod_data["Entities"] == ent]
-                extr = extr.groupby(column).mean()
+                extr = mod_data[mod_data[line_column] == ent]
+                extr = extr.groupby(x_column).mean()
 
-                line = linesEntities[ent]
+                line = line_styles[ent]
                 ax.plot(
                     extr.index,
                     extr["Time"],
@@ -84,7 +95,7 @@ if __name__ == "__main__":
                     linewidth=line[1],
                     color=colors[model],
                     markersize=3,
-                    label=model if ent == 100000 else None,
+                    label=model if ent == count[-2] else None,
                     zorder=1,
                 )
 
@@ -102,17 +113,17 @@ if __name__ == "__main__":
                         zorder=10,
                     )
 
-        for ent in reversed(entities):
-            line = linesEntities[ent]
+        for ent in reversed(values[line_column]):
+            line = line_styles[ent]
             ax.plot(
                 [0],
                 [0],
                 linestyle=line[0],
                 linewidth=line[1],
                 color="black",
-                label=f"{ent//1000}k entities",
+                label=f"{ent//1000}k {line_column}",
             )
-        for ent in reversed(sec_values):
+        for ent in reversed(values[marker_column]):
             m = markers[ent]
             ax.scatter(
                 [0],
@@ -133,6 +144,6 @@ if __name__ == "__main__":
         leg.set(zorder=20)
 
         fig.tight_layout()
-        fig.savefig(f"results-{column}.svg")
+        fig.savefig(f"results-{line_column}{x_column}.svg")
 
     plt.show()
