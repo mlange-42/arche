2233bcb72697b714bca925cc601cf44df916ab8d
Martin Lange <martin.lange@ufz.de>
2024-01-16 23:35:06 +0100

    extend standalone events example

--- examples/events/main.go
+++ examples/events/main.go
@@ -1,5 +1,4 @@
-// Demonstrates the use of a EntityEvent listener,
-// notifying on changes to the component composition of entities.
+// Demonstrates the use of a EntityEvent through listeners.
 package main
 
 import (
@@ -22,38 +21,70 @@ type Velocity struct {
 	Y float64
 }
 
-// Rotation component
-type Rotation struct {
-	A float64
+// ChildOf relation component
+type ChildOf struct {
+	ecs.Relation
+}
+
+// EventHandler that prints information about received events.
+type EventHandler struct{}
+
+// Notify is called with all subscribed events.
+func (h *EventHandler) Notify(world *ecs.World, evt ecs.EntityEvent) {
+	fmt.Printf("    Type mask:     %06b\n", evt.EventTypes)
+	fmt.Printf("    Entity:        %+v\n", evt.Entity)
+	fmt.Printf("    Added/Removed: %+v / %+v\n", evt.Added, evt.Removed)
+	fmt.Printf("    Relation:      %+v -> %+v\n", evt.OldRelation, evt.NewRelation)
+
+	var target ecs.Entity
+	if evt.NewRelation != nil {
+		target = world.Relations().Get(evt.Entity, *evt.NewRelation)
+	}
+	fmt.Printf("    Target:        %+v -> %+v\n", evt.OldTarget, target)
 }
 
 func main() {
 	// Create a World.
 	world := ecs.NewWorld()
-	listener := listener.NewCallback(
-		func(world *ecs.World, evt ecs.EntityEvent) {
-			fmt.Printf("Events: %08b Added: %v Removed: %v\n", evt.EventTypes, evt.Added, evt.Removed)
-		},
-		event.Entities|event.Components,
-	)
 
 	// Get component IDs
 	posID := ecs.ComponentID[Position](&world)
 	velID := ecs.ComponentID[Velocity](&world)
-	rotID := ecs.ComponentID[Rotation](&world)
+	childID := ecs.ComponentID[ChildOf](&world)
+
+	// Create the event handler.
+	handler := EventHandler{}
+
+	// Create the listener.
+	listener := listener.NewCallback(
+		// Function to handle events.
+		handler.Notify,
+		// Subscribe to event types; could also use `event.All` here.
+		event.EntityCreated|event.EntityRemoved|
+			event.ComponentAdded|event.ComponentRemoved|
+			event.RelationChanged|event.TargetChanged,
+		// Optionally, restrict subscription to a list of component types / IDs.
+		//posID, childID,
+	)
 
 	// Register a listener function.
 	world.SetListener(&listener)
 
-	// Create/manipulate/delete entities and observe the listener's output
-	e0 := world.NewEntity(posID)
-	e1 := world.NewEntity(posID, velID)
+	fmt.Println("=== Create entity ===")
+	child := world.NewEntity()
 
-	world.Add(e0, velID)
-	world.Add(e1, rotID)
+	fmt.Println("=== Create entity with normal components ===")
+	parent := world.NewEntity(posID, velID)
 
-	world.Remove(e1, posID, velID)
+	fmt.Println("=== Create entity with relation component ===")
+	world.NewEntity(childID)
 
-	world.RemoveEntity(e0)
-	world.RemoveEntity(e1)
+	fmt.Println("=== Add normal component to entity ===")
+	world.Add(child, posID)
+
+	fmt.Println("=== Add relation component to entity ===")
+	world.Add(child, childID)
+
+	fmt.Println("=== Change relation target ===")
+	world.Relations().Set(child, childID, parent)
 }
