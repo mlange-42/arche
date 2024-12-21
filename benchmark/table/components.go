c6fd51ce582625269c98cd677330e87033444ff4
mlange-42 <martin_lange_@gmx.net>
2024-12-22 00:30:06 +0100

    add benchmarks for Assign and NewWith

--- benchmark/table/components.go
+++ benchmark/table/components.go
@@ -15,6 +15,9 @@ func benchesComponents() []bench {
 		{Name: "World.Remove 5 Comps", Desc: "", F: componentsRemove5_1000, N: 1000},
 
 		{Name: "World.Exchange 1 Comp", Desc: "memory already allocated", F: componentsExchange1_1000, N: 1000},
+
+		{Name: "World.Assign 1 Comp", Desc: "memory already allocated", F: componentsAssign1_1000, N: 1000},
+		{Name: "World.Assign 5 Comps", Desc: "memory already allocated", F: componentsAssign5_1000, N: 1000},
 	}
 }
 
@@ -154,5 +157,71 @@ func componentsExchange1_1000(b *testing.B) {
 		}
 		b.StopTimer()
 		w.Batch().Exchange(filter, ex1, ex2)
+	}
+}
+
+func componentsAssign1_1000(b *testing.B) {
+	b.StopTimer()
+
+	w := ecs.NewWorld(ecs.NewConfig().WithCapacityIncrement(1024))
+	id1 := ecs.ComponentID[comp1](&w)
+	filter := ecs.All(id1)
+
+	builder := ecs.NewBuilder(&w)
+	query := builder.NewBatchQ(1000)
+
+	entities := make([]ecs.Entity, 0, 1000)
+	for query.Next() {
+		entities = append(entities, query.Entity())
+	}
+
+	comp1 := ecs.Component{ID: id1, Comp: &comp1{}}
+
+	for i := 0; i < b.N; i++ {
+		b.StartTimer()
+		for _, e := range entities {
+			w.Assign(e, comp1)
+		}
+		b.StopTimer()
+		w.Batch().Remove(filter, id1)
+	}
+}
+
+func componentsAssign5_1000(b *testing.B) {
+	b.StopTimer()
+
+	w := ecs.NewWorld(ecs.NewConfig().WithCapacityIncrement(1024))
+	id1 := ecs.ComponentID[comp1](&w)
+	id2 := ecs.ComponentID[comp2](&w)
+	id3 := ecs.ComponentID[comp3](&w)
+	id4 := ecs.ComponentID[comp4](&w)
+	id5 := ecs.ComponentID[comp5](&w)
+
+	ids := []ecs.ID{id1, id2, id3, id4, id5}
+	filter := ecs.All(ids...)
+
+	builder := ecs.NewBuilder(&w)
+	query := builder.NewBatchQ(1000)
+
+	entities := make([]ecs.Entity, 0, 1000)
+	for query.Next() {
+		entities = append(entities, query.Entity())
+	}
+
+	comps := []ecs.Component{
+		{ID: id1, Comp: &comp1{}},
+		{ID: id2, Comp: &comp2{}},
+		{ID: id3, Comp: &comp3{}},
+		{ID: id4, Comp: &comp4{}},
+		{ID: id5, Comp: &comp5{}},
+	}
+
+	for i := 0; i < b.N; i++ {
+		b.StartTimer()
+		for _, e := range entities {
+			w.Assign(e, comps...)
+		}
+		b.StopTimer()
+		w.Batch().Remove(filter, ids...)
 	}
 }
